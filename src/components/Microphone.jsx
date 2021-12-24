371c3ae87a6f70e58121d0a334e3b4275addc842
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-12-24 23:19:56 +0530

    Component addition

--- src/components/Microphone.jsx
+++ src/components/Microphone.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsMicFill } from "react-icons/bs";
 import { BsMicMuteFill } from "react-icons/bs";
 const Microphone = () => {
+  const [checkPlayBtn, setCheckPlayButton] = useState("play");
+
   return (
     <>
       <div>
         <h1>This is Microphone Component.</h1>
+        <div></div>
+        <BsMicFill />
       </div>
     </>
   );
