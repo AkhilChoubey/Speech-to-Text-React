facae9f62f9e96bb6896f02aaf184827a143524a
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-12-24 22:56:00 +0530

    Component addition

---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TextDisplay = () => {
+  return (
+    <>
+      <div>
+        <h1>This is TextDisplay Component.</h1>
+      </div>
+    </>
+  );
+};
+
+export default TextDisplay;
