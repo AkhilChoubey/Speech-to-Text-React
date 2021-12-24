7da8a5cf2396d1c5a2a5ffc39688c9e2a5069acb
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-12-24 23:11:39 +0530

    Component addition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Microphone from "./Microphone";
+import TextDisplay from "./TextDisplay";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 const Home = () => {
   return (
     <>
       <div>
         <h1>This is Home Component.</h1>
+        <ThemeSwitcher />
+        <Microphone />
+        <TextDisplay />
       </div>
     </>
   );
