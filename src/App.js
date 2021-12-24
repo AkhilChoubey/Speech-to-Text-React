5804730e02ac9b8006f547c9a1f15559f7650c2c
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-12-24 23:09:52 +0530

    Component addition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
