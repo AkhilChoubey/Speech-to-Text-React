4fa0e894da5510bca6e94c32fef33b4298684934
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-12-26 15:27:03 +0530

    Microphone feature addition

--- src/components/TextDisplay.jsx
+++ src/components/TextDisplay.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import Microphone from "./Microphone";
 const TextDisplay = () => {
   return (
     <>
       <div>
         <h1>This is TextDisplay Component.</h1>
+        {Microphone.transcript && (
+          <div className="microphone-result-container">
+            <div className="microphone-result-text">
+              {Microphone.transcript}
+            </div>
+            <button
+              className="microphone-reset btn"
+              onClick={Microphone.handleReset}
+            >
+              Reset
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
