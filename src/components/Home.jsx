e0f7d94a8e689b801bfe6757ab3a31a1171c4cd8
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2022-01-09 17:20:10 +0530

    ui changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Microphone from "./Microphone";
 import TextDisplay from "./TextDisplay";
 import ThemeSwitcher from "./ThemeSwitcher";
+import { BsGithub } from "react-icons/bs";
 
 const Home = () => {
   return (
@@ -11,6 +12,30 @@ const Home = () => {
         <div className="root-container">
           <Microphone />
           <TextDisplay />
+        </div>
+        <div
+          style={{
+            width: "20rem",
+            bottom: "0",
+            position: "absolute",
+            /* padding-top: 0.5%; */
+            textAlign: "end",
+            /* height: 5rem; */
+          }}
+        >
+          <a
+            type="button"
+            style={{
+              border: "1px solid rgb(5 5 5)",
+              textDecoration: "none",
+              fontSize: "1.5rem",
+              padding: "1.5%",
+              borderRadius: "25px 25px 0 0",
+            }}
+            href="https://github.com/AkhilChoubey"
+          >
+            Know the Develoer <BsGithub />
+          </a>
         </div>
       </div>
     </>
